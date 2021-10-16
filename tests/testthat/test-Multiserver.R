6fe668cd6ff6d703d96bd1e49f2e0a03986993ca
Caitlin Apcar <caitlin.apcar@students.mq.edu.au>
2021-10-16 16:15:46 +1100

    updates to README

--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -2,3 +2,5 @@ test_that("Multiserver Works", {
   expect_error(Multiserver()) #does the package show an error message
   expect_output(Multiserver(5, 10, 2)) #does the package print an output to the console
 })
+
+devtools::test()
