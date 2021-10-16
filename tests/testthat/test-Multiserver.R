9dd6aac4df5292905cc06fb8205179b72377a73b
Caitlin Apcar <caitlin.apcar@students.mq.edu.au>
2021-10-16 15:27:30 +1100

    devtooks :: document()

--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -1,4 +1,4 @@
 test_that("Multiserver Works", {
-  expect_error(Multiserver())
-  expect_output(Multiserver(5, 10, 2))
+  expect_error(Multiserver()) #does the package show an error message
+  expect_output(Multiserver(5, 10, 2)) #does the package print an output to the console
 })
